3974c2d9b481fe97f30bdc73a78ad877aeeb349a
solomon stephen <33923021+realrufans@users.noreply.github.com>
2022-10-12 17:57:42 +0100

    logout button

--- components/header.js
+++ components/header.js
@@ -96,27 +96,16 @@ function Header() {
                 <ChevronDownIcon className="h-6" />
               </div>
               <div className=" absolute top-9 -right-4  hidden  h-80 w-64  space-y-3    border-[2px]  border-white/50 bg-coinSinoPurpleNav text-center  text-coinSinoTextColor2     hover:inline-block hover:text-coinSinoPink group-hover:inline-block sm:w-80">
-                {[2].map((e, i) => (
+                {[2].map((e,i) => (
                   <div
-                    key={i}
+                  key={i}
                     onClick={disConnectWallet}
                     className="flex items-center justify-between border-b-[1px] border-coinSinoTextColor2  p-2 hover:bg-coinSinoTextColor2/20"
                   >
                     <p className="p-2 text-lg font-semibold">Disconnect</p>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="h-6 w-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-                      />
-                    </svg>
+
+                    <p className="   h-6 w-6   bg-[url('/images/logoutIcon.svg')] bg-no-repeat   ">
+                     </p>
                   </div>
                 ))}
               </div>
