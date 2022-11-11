a5a33891b3bdcaf53e3e46c4de9bffc7e6c91367
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-11-11 13:19:07 +0100

    add health check route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,36 +1,3 @@
-// import jwt from "jsonwebtoken";
-// import { app, database } from "./Firebase";
-// import { doc, getDoc } from "firebase/firestore";
-
-// export default async function handler(req, res) {
-//   try {
-//     const authorization_key = req.headers.authorization;
-//     const docRef = doc(database, "authorization", authorization_key);
-//     const docSnap = await getDoc(docRef);
-
-//     // sign data
-//     const signature = jwt.sign(docSnap.data(), process.env.jwt_secret);
-
-//     return res.status(200).json({
-//       data: docSnap.data(),
-//       signature,
-//     });
-//   } catch (error) {
-//     return res.status(400).json({ Error: "Something went wrong" });
-//   }
-//   console.log(
-//     "-----------------------------------------------------------------------------"
-//   );
-
-//   const value = req.headers.accesstoken;
-//   console.log(value);
-
-//   const decode = jwt.decode(value);
-//   console.log("Decoded: ", decode);
-
-//   res.status(200).json({
-//     opkey: process.env.opkey,
-//     injkey: process.env.injkey,
-//     treakey: process.env.treakey,
-//   });
-// }
+export default async function handler(req, res) {
+  return "Reached the SINO endpoint";
+}
